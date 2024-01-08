1dd20e626a7f6818f0b7706d8cf6108195f79f5c
Igor Gonzalez Dourado <igorgonzalezdourado@gmail.com>
2024-01-08 22:28:21 +0100

    sistemalogin

--- db_connect.php
+++ db_connect.php
@@ -9,6 +9,8 @@ $connect = mysqli_connect($servername,$username,$password,$db_name);
 
 if(mysqli_connect_error()):
     echo "Falha na conexão".mysqli_connect_error();
+else:
+    echo "";
 endif;
 
 
